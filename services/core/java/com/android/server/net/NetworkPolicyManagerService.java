696ad617bb39d5baa5fef3b9c63fa473df659839
Tommy Webb <tommy@calyxinstitute.org>
2023-10-11 11:22:58 -0400

    NPMS: Remove support for SDK sandbox UIDs
    Remove all code related to SDK sandbox UIDs from NPMS, with a goal of
    making it so that SDK sandbox UIDs have no network access, particularly
    due to not being added to the restricted mode allowlist.
    
    Has corresponding Connectivity change with same Change-Id.
    
    Tests not updated.
    
    Issue: calyxos#1493
    Change-Id: Ib0483df8471b2df918b2548118faa9bf80baae90

--- services/core/java/com/android/server/net/NetworkPolicyManagerService.java
+++ services/core/java/com/android/server/net/NetworkPolicyManagerService.java
@@ -6777,11 +6777,6 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
         try {
             mNetworkManager.setUidOnMeteredNetworkDenylist(uid, enable);
             mLogger.meteredDenylistChanged(uid, enable);
-            if (Process.isApplicationUid(uid)) {
-                final int sdkSandboxUid = Process.toSdkSandboxUid(uid);
-                mNetworkManager.setUidOnMeteredNetworkDenylist(sdkSandboxUid, enable);
-                mLogger.meteredDenylistChanged(sdkSandboxUid, enable);
-            }
         } catch (IllegalStateException e) {
             Log.wtf(TAG, "problem setting denylist (" + enable + ") rules for " + uid, e);
         } catch (RemoteException e) {
@@ -6794,11 +6789,6 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
         try {
             mNetworkManager.setUidOnMeteredNetworkAllowlist(uid, enable);
             mLogger.meteredAllowlistChanged(uid, enable);
-            if (Process.isApplicationUid(uid)) {
-                final int sdkSandboxUid = Process.toSdkSandboxUid(uid);
-                mNetworkManager.setUidOnMeteredNetworkAllowlist(sdkSandboxUid, enable);
-                mLogger.meteredAllowlistChanged(sdkSandboxUid, enable);
-            }
         } catch (IllegalStateException e) {
             Log.wtf(TAG, "problem setting allowlist (" + enable + ") rules for " + uid, e);
         } catch (RemoteException e) {
@@ -6891,11 +6881,6 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
             try {
                 mNetworkManager.setFirewallUidRule(chain, uid, rule);
                 mLogger.uidFirewallRuleChanged(chain, uid, rule);
-                if (Process.isApplicationUid(uid)) {
-                    final int sdkSandboxUid = Process.toSdkSandboxUid(uid);
-                    mNetworkManager.setFirewallUidRule(chain, sdkSandboxUid, rule);
-                    mLogger.uidFirewallRuleChanged(chain, sdkSandboxUid, rule);
-                }
             } catch (IllegalStateException e) {
                 Log.wtf(TAG, "problem setting firewall uid rules", e);
             } catch (RemoteException e) {
@@ -6963,9 +6948,6 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
             Log.wtf(TAG, "problem resetting firewall uid rules for " + uid, e);
         } catch (RemoteException e) {
             // ignored; service lives in system_server
-        }
-        if (Process.isApplicationUid(uid)) {
-            resetUidFirewallRules(Process.toSdkSandboxUid(uid));
         }
     }
 
