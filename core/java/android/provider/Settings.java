f78e3d9ef2a2b14e755d5296b7632753e3f67f9a
cjh1249131356 <cjh1249131356@gmail.com>
2024-01-06 21:48:51 +0800

    SystemUI: Implement RefreshRateManager [1/2]
    Includes:
    - Per-app refresh rate config
    - Extreme refresh rate (Force all apps to run in maximum refresh rate)
    - Temp refresh rate allowed (Used in MEMC)
    - Refresh rate QS tile
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    [nurkeinneid: move to fwb and drop sdk extensions/dependencies]
    Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7413,6 +7413,18 @@ public final class Settings {
         public static final String STATUSBAR_COMBINED_NOTIF_COUNT = "statusbar_combined_notif_count";
 
         /**
+         * Per-app refresh rate config
+         * @hide
+         */
+        public static final String REFRESH_RATE_CONFIG_CUSTOM = "refresh_rate_config_custom";
+
+        /**
+         * Force highest refresh rate in all apps
+         * @hide
+         */
+        public static final String EXTREME_REFRESH_RATE = "extreme_refresh_rate";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
