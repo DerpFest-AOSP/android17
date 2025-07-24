01e449b7807a57791d2db6a5c41e1329e01e5f67
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-07-25 00:53:17 +0530

    SystemUI: Alter constraints for privacy chip on large screen header too
    Large screen header is used on landscape mode and without this in
    landscape the status icons don't reappear even after the privacy
    chip goes away.
    
    Change-Id: I206a5d829584f872fbef1739244912d9b5b37813

--- packages/SystemUI/src/com/android/systemui/shade/CombinedShadeHeadersConstraintManagerImpl.kt
+++ packages/SystemUI/src/com/android/systemui/shade/CombinedShadeHeadersConstraintManagerImpl.kt
@@ -31,6 +31,9 @@ object CombinedShadeHeadersConstraintManagerImpl : CombinedShadeHeadersConstrain
         return ConstraintsChanges(
             qqsConstraintsChanges = {
                 setVisibility(R.id.shade_header_system_icons, headerIconsVisibility)
+            },
+            largeScreenConstraintsChanges = {
+                setVisibility(R.id.shade_header_system_icons, headerIconsVisibility)
             }
         )
     }
