5e758b6a0246b9ec355649961d7ac8f52d4c48a6
Ghosuto <clash.raja10@gmail.com>
2025-10-19 08:36:04 +0000

    EdgeLight: Introduce EdgeLight animation style
    Change-Id: I133b28619d00254488815660e70e86c8f754aebc
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7269,12 +7269,6 @@ public final class Settings {
         public static final String EDGE_LIGHT_PULSE_COUNT = "edge_light_pulse_count";
 
         /**
-         * IMPORTANT: If you add a new public settings you also have to add it to
-         * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
-         * it to PRIVATE_SETTINGS below. Also add a validator that can validate
-         * the setting value. See an example above.
-         */
-        /**
          * Stroke width for edge light
          * @hide
          */
@@ -7286,6 +7280,18 @@ public final class Settings {
          */
         public static final String EDGE_LIGHT_STYLE = "edge_light_style";
 
+        /**
+         * Edge light animation effect type
+         * @hide
+         */
+        public static final String EDGE_LIGHT_ANIMATION_EFFECT = "edge_light_animation_effect";
+
+        /**
+         * IMPORTANT: If you add a new public settings you also have to add it to
+         * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
+         * it to PRIVATE_SETTINGS below. Also add a validator that can validate
+         * the setting value. See an example above.
+         */
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
