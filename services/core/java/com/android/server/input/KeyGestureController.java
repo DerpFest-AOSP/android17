yaap/frameworks_base

KeyGestureController: Adapt our hash mute gesture to A16/QPR2

services/core/java/com/android/server/input/KeyGestureController.java
@@ -454,7 +454,7 @@ final class KeyGestureController {
                     public boolean preCondition() {
                         switch (mPowerVolUpBehavior) {
                             case POWER_VOLUME_UP_BEHAVIOR_MUTE:
-                                return !mRingerToggleChord.equals(Settings.Secure.VOLUME_HUSH_OFF);
+                                return !mRingerToggleChord.equals(Settings.Secure.YAAP_VOLUME_HUSH_OFF);
                             default:
                                 return true;
                         }

DerpFest-AOSP/android17

services/core/java/com/android/server/input/KeyGestureController.java
@@ -424,7 +424,7 @@ final class KeyGestureController {
                     public boolean preCondition() {
                         switch (mPowerVolUpBehavior) {
                             case POWER_VOLUME_UP_BEHAVIOR_MUTE:
-                                return !mRingerToggleChord.equals(Settings.Secure.VOLUME_HUSH_OFF);
+                                return !mRingerToggleChord.equals(Settings.Secure.DERP_VOLUME_HUSH_OFF);
                             default:
                                 return true;
                         }