33ecf7d131afc90b16b72577f00c09e2a381cfd3
NurKeinNeid <nurkeinneid@derpfest.org>
2026-02-04 01:22:37 +0100

    Revert "Only enable ambient wallpaper if AOD is also enabled"
    This reverts commit 8a06307a697436e0974a5e0d6e4c156e1766e153.
    
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- packages/SystemUI/multivalentTests/src/com/android/systemui/wallpapers/data/repository/WallpaperRepositoryImplTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/wallpapers/data/repository/WallpaperRepositoryImplTest.kt
@@ -236,7 +236,6 @@ class WallpaperRepositoryImplTest : SysuiTestCase() {
     fun wallpaperSupportsAmbientMode_deviceDoesNotSupport_false() =
         testScope.runTest {
             underTest = kosmos.wallpaperRepository
-            secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON, 1)
             secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON_WALLPAPER_ENABLED, 1)
             context.orCreateTestableResources.addOverride(
                 R.bool.config_dozeSupportsAodWallpaper,
@@ -251,7 +250,6 @@ class WallpaperRepositoryImplTest : SysuiTestCase() {
     fun wallpaperSupportsAmbientMode_sysuiOverrideFalse_deviceDoesSupport_false() =
         testScope.runTest {
             underTest = kosmos.wallpaperRepository
-            secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON, 1)
             secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON_WALLPAPER_ENABLED, 1)
             context.orCreateTestableResources.addOverride(
                 R.bool.config_dozeSupportsAodWallpaper,
@@ -270,7 +268,6 @@ class WallpaperRepositoryImplTest : SysuiTestCase() {
     fun wallpaperSupportsAmbientMode_sysuiOverrideTrue_deviceDoesSupport_true() =
         testScope.runTest {
             underTest = kosmos.wallpaperRepository
-            secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON, 1)
             secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON_WALLPAPER_ENABLED, 1)
             context.orCreateTestableResources.addOverride(
                 R.bool.config_dozeSupportsAodWallpaper,
@@ -289,7 +286,6 @@ class WallpaperRepositoryImplTest : SysuiTestCase() {
     fun wallpaperSupportsAmbientMode_sysuiOverrideNull_deviceDoesSupport_true() =
         testScope.runTest {
             underTest = kosmos.wallpaperRepository
-            secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON, 1)
             secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON_WALLPAPER_ENABLED, 1)
             context.orCreateTestableResources.addOverride(
                 R.bool.config_dozeSupportsAodWallpaper,
@@ -308,7 +304,6 @@ class WallpaperRepositoryImplTest : SysuiTestCase() {
     fun wallpaperSupportsAmbientMode_deviceDoesSupport_true() =
         testScope.runTest {
             underTest = kosmos.wallpaperRepository
-            secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON, 1)
             secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON_WALLPAPER_ENABLED, 1)
             context.orCreateTestableResources.addOverride(
                 R.bool.config_dozeSupportsAodWallpaper,
@@ -331,28 +326,12 @@ class WallpaperRepositoryImplTest : SysuiTestCase() {
     fun wallpaperSupportsAmbientMode_deviceDoesSupport_settingDisabled_false() =
         testScope.runTest {
             underTest = kosmos.wallpaperRepository
-            secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON, 1)
             secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON_WALLPAPER_ENABLED, 0)
             context.orCreateTestableResources.addOverride(
                 R.bool.config_dozeSupportsAodWallpaper,
                 true,
             )
 
-            val latest by collectLastValue(underTest.wallpaperSupportsAmbientMode)
-            assertThat(latest).isFalse()
-        }
-
-    @Test
-    fun wallpaperSupportsAmbientMode_deviceDoesSupport_userDisabledAod_false() =
-        testScope.runTest {
-            underTest = kosmos.wallpaperRepository
-            secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON, 0)
-            secureSettings.putInt(Settings.Secure.DOZE_ALWAYS_ON_WALLPAPER_ENABLED, 1)
-            context.orCreateTestableResources.addOverride(
-                R.bool.config_dozeSupportsAodWallpaper,
-                false,
-            )
-            configRepository.onAnyConfigurationChange()
             val latest by collectLastValue(underTest.wallpaperSupportsAmbientMode)
             assertThat(latest).isFalse()
         }
