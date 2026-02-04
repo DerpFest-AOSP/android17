04ed8afcb7f6ac4492fd67f52997be55ff86da3b
NurKeinNeid <nurkeinneid@derpfest.org>
2026-02-04 02:28:49 +0100

    SystemUI: Update bouncer visibility logic to respect secure window setting
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java
@@ -504,7 +504,9 @@ public class NotificationShadeWindowControllerImpl
                     "display_max_refresh_rate", (long) mLpChanged.preferredMaxDisplayRefreshRate);
         }
 
-        if (state.bouncerShowing) {
+        boolean ignoreSecure = Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.WINDOW_IGNORE_SECURE, 0) == 1;
+        if (state.bouncerShowing && !ignoreSecure) {
             mLpChanged.flags |= LayoutParams.FLAG_SECURE;
         } else {
             mLpChanged.flags &= ~LayoutParams.FLAG_SECURE;
