zenzer0s-alioth/frameworks_base

AppLock: Allow locking all apps visible in launcher
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

services/applock/java/com/android/server/app/AppLockManagerService.kt
@@ -50,6 +50,7 @@ import android.util.Slog
 
 import com.android.internal.R
 import com.android.internal.annotations.GuardedBy
+import com.android.internal.util.custom.Utils
 import com.android.server.LocalServices
 import com.android.server.SystemService
 import com.android.server.app.AppLockManagerServiceInternal
@@ -494,6 +495,7 @@ class AppLockManagerService(
                 userId
             )
             if (!aInfo.isSystemApp()) return
+            if (Utils.launchablePackages(context).contains(pkg)) return
             if (!whiteListedSystemApps.contains(pkg))
                 throw IllegalArgumentException("System package $pkg is not whitelisted")
         } catch(e: PackageManager.NameNotFoundException) {

DerpFest-AOSP/android17

services/applock/java/com/android/server/app/AppLockManagerService.kt
@@ -50,6 +50,7 @@ import android.util.Slog
 
 import com.android.internal.R
 import com.android.internal.annotations.GuardedBy
+import com.android.internal.util.derp.derpUtils
 import com.android.server.LocalServices
 import com.android.server.SystemService
 import com.android.server.app.AppLockManagerServiceInternal
@@ -494,6 +495,7 @@ class AppLockManagerService(
                 userId
             )
             if (!aInfo.isSystemApp()) return
+            if (Utils.launchablePackages(context).contains(pkg)) return
             if (!whiteListedSystemApps.contains(pkg))
                 throw IllegalArgumentException("System package $pkg is not whitelisted")
         } catch(e: PackageManager.NameNotFoundException) {