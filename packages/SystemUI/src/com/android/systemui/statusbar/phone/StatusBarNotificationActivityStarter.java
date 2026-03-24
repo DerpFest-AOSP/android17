9ec8b5caad3e40be1e200c641f7d36eee1f5ad7f
Dmitrii <bankersenator@gmail.com>
2026-03-24 10:03:21 +0000

    SystemUI: Toggle to launch notification apps in bubble [1/2]
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarNotificationActivityStarter.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarNotificationActivityStarter.java
@@ -419,10 +419,17 @@ public class StatusBarNotificationActivityStarter implements NotificationActivit
                     remoteInputText.toString());
         }
         final boolean canBubble = entry.canBubble();
-        if (canBubble) {
+        boolean forceBubbleApps = Settings.System.getIntForUser(mContext.getContentResolver(),
+                "notification_bubble_apps", 0, UserHandle.USER_CURRENT) == 1;
+
+        if (canBubble || forceBubbleApps) {
             mLogger.logExpandingBubble(entry);
             removeHunAfterClick(row);
-            expandBubbleStackOnMainThread(entry);
+            if (forceBubbleApps && !entry.isBubble()) {
+                expandAppBubbleStackOnMainThread(intent, entry.getSbn().getUser());
+            } else {
+                expandBubbleStackOnMainThread(entry);
+            }
         } else {
             startNotificationIntent(intent, fillInIntent, entry, row, animate, isActivityIntent);
         }
@@ -505,6 +512,23 @@ public class StatusBarNotificationActivityStarter implements NotificationActivit
 
     private void expandBubbleStack(NotificationEntry entry) {
         mBubblesManagerOptional.get().expandStackAndSelectBubble(entry);
+        mShadeController.collapseShade();
+    }
+
+    private void expandAppBubbleStackOnMainThread(PendingIntent intent, UserHandle user) {
+        if (!mBubblesManagerOptional.isPresent()) {
+            return;
+        }
+
+        if (Looper.getMainLooper().isCurrentThread()) {
+            expandAppBubbleStack(intent, user);
+        } else {
+            mMainThreadHandler.post(() -> expandAppBubbleStack(intent, user));
+        }
+    }
+
+    private void expandAppBubbleStack(PendingIntent intent, UserHandle user) {
+        mBubblesManagerOptional.get().expandStackAndSelectBubble(intent, user);
         mShadeController.collapseShade();
     }
 
