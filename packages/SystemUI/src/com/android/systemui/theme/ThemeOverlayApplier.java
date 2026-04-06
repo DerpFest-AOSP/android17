fdb77cb7b68e0e1af85815155de846a4c69a1ba1
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-04-06 20:51:33 +0800

    [ax_theme_engine] adding back gesture | charging anim | battery themes support
    Change-Id: Id4e5ab155a3b90c7a2e8c943e79f1abd89320abf
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -136,6 +136,15 @@ public class ThemeOverlayApplier implements Dumpable {
     static final String OVERLAY_CATEGORY_UI_STYLE_SYSUI =
             "android.theme.customization.style.systemui";
 
+    static final String OVERLAY_CATEGORY_BACK_GESTURE =
+            "android.theme.customization.back_gesture";
+
+    static final String OVERLAY_CATEGORY_CHARGING_ANIMATION =
+            "android.theme.customization.charging_animation";
+
+    static final String OVERLAY_CATEGORY_BATTERY_STYLE =
+            "android.theme.customization.battery_style";
+
     /*
      * All theme customization categories used by the system, in order that they should be applied,
      * starts with launcher and grouped by target package.
@@ -154,7 +163,10 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_SYSUI,
             OVERLAY_CATEGORY_ICON_SETTINGS,
             OVERLAY_CATEGORY_ICON_THEME_PICKER,
-            OVERLAY_CATEGORY_DISPLAY_CUTOUT);
+            OVERLAY_CATEGORY_DISPLAY_CUTOUT,
+            OVERLAY_CATEGORY_BACK_GESTURE,
+            OVERLAY_CATEGORY_CHARGING_ANIMATION,
+            OVERLAY_CATEGORY_BATTERY_STYLE);
 
     /* Categories that need to be applied to the current user as well as the system user. */
     @VisibleForTesting
@@ -168,7 +180,10 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_UI_STYLE_SYSUI,
             OVERLAY_CATEGORY_ICON_ANDROID,
             OVERLAY_CATEGORY_ICON_SYSUI,
-            OVERLAY_CATEGORY_DISPLAY_CUTOUT);
+            OVERLAY_CATEGORY_DISPLAY_CUTOUT,
+            OVERLAY_CATEGORY_BACK_GESTURE,
+            OVERLAY_CATEGORY_CHARGING_ANIMATION,
+            OVERLAY_CATEGORY_BATTERY_STYLE);
 
     /* Allowed overlay categories for each target package. */
     private final Map<String, Set<String>> mTargetPackageToCategories = new ArrayMap<>();
@@ -199,7 +214,8 @@ public class ThemeOverlayApplier implements Dumpable {
                 OVERLAY_CATEGORY_UI_STYLE_ANDROID,
                 OVERLAY_CATEGORY_ICON_ANDROID, OVERLAY_CATEGORY_DISPLAY_CUTOUT));
         mTargetPackageToCategories.put(SYSUI_PACKAGE,
-                Sets.newHashSet(OVERLAY_CATEGORY_ICON_SYSUI, OVERLAY_CATEGORY_UI_STYLE_SYSUI));
+                Sets.newHashSet(OVERLAY_CATEGORY_ICON_SYSUI, OVERLAY_CATEGORY_UI_STYLE_SYSUI, OVERLAY_CATEGORY_BACK_GESTURE,
+                        OVERLAY_CATEGORY_CHARGING_ANIMATION, OVERLAY_CATEGORY_BATTERY_STYLE));
         mTargetPackageToCategories.put(SETTINGS_PACKAGE,
                 Sets.newHashSet(OVERLAY_CATEGORY_ICON_SETTINGS, OVERLAY_CATEGORY_UI_STYLE_SETTINGS));
         mTargetPackageToCategories.put(mLauncherPackage,
@@ -216,6 +232,9 @@ public class ThemeOverlayApplier implements Dumpable {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_ANDROID, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_DISPLAY_CUTOUT, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_SYSUI, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_BACK_GESTURE, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_CHARGING_ANIMATION, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_BATTERY_STYLE, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_SETTINGS, SETTINGS_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_LAUNCHER, mLauncherPackage);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_THEME_PICKER, mThemePickerPackage);
