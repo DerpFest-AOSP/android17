018aa144166006e630a9541307c7beb0268f61cd
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-04-12 18:09:14 +0200

    [ax_dynamic_bar] limiting status bar chip width
    Change-Id: Iec04c3b59cfb0200ddd33e7ee92c212a1a6f44ae

--- packages/SystemUI/src/com/android/systemui/axdynamicbar/ui/compose/AxDynamicBarChip.kt
+++ packages/SystemUI/src/com/android/systemui/axdynamicbar/ui/compose/AxDynamicBarChip.kt
@@ -53,6 +53,7 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalViewConfiguration
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import android.graphics.drawable.Drawable
@@ -96,6 +97,7 @@ fun AxDynamicBarChip(
     val keyguardCarrier by viewModel.keyguardCarrierText.collectAsStateWithLifecycle()
     
     val carrierName = if (isOnKeyguard && ignoreKeyguard) keyguardCarrier.takeIf { it.isNotBlank() } else null
+    val chipTextMaxWidth = dimensionResource(R.dimen.ongoing_activity_chip_max_text_width)
     val screenWidthPx = with(LocalDensity.current) {
         LocalConfiguration.current.screenWidthDp.dp.toPx()
     }
@@ -282,7 +284,7 @@ fun AxDynamicBarChip(
                                         color = contentColor,
                                         maxLines = 1,
                                         overflow = TextOverflow.Ellipsis,
-                                        modifier = Modifier.widthIn(max = 100.dp).basicMarquee(iterations = 1),
+                                        modifier = Modifier.widthIn(max = chipTextMaxWidth).basicMarquee(iterations = 1),
                                     )
                                 }
                             }
@@ -330,7 +332,7 @@ fun AxDynamicBarChip(
                             ) { event ->
                                 PillEventText(
                                     event,
-                                    Modifier.widthIn(max = 88.dp),
+                                    Modifier.widthIn(max = chipTextMaxWidth),
                                     overrideColor = contentColor,
                                 )
                             }
