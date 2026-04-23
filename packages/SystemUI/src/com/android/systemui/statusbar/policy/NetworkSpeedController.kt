13f5d4fbb8e33f40c25f7a72a1e4bf1be142e70e
NurKeinNeid <nurkeinneid@derpfest.org>
2026-04-23 19:32:02 +0200

    SystemUI: Update network speed refresh interval to 1 second
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkSpeedController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkSpeedController.kt
@@ -216,7 +216,7 @@ class NetworkSpeedController private constructor(
 
         const val TAG = "NetworkSpeedController"
         const val ICON_HIDE_LIST = "icon_blacklist"
-        const val REFRESH_INTERVAL_MS = 4000L
+        const val REFRESH_INTERVAL_MS = 1000L
         const val AUTOHIDE_THRESHOLD = 1024L // 1KB
     }
 }
