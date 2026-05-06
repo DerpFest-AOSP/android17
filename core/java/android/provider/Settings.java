e7674769448fddd123ca87920035a61a229e8940
Jackeagle <jackeagle102@gmail.com>
2026-05-06 17:30:16 +0200

    SystemUI: Add selectable charging animations
    Enable the built-in charging ripple and add a few extra
    animation styles that play when you plug in the charger.
    Pick your style from Blissify under Appearance.
    
    Change-Id: Ie8c76a8838157f456dd6ee1548aff888b7d254a6
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7586,6 +7586,13 @@ public final class Settings {
         public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
 
         /**
+         * Charging animation style
+         * 0 = None, 1 = Ripple, 2 = Flash, 3 = Glow Pulse, 4 = Battery Fill
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION_STYLE = "charging_animation_style";
+
+        /**
          * Defines the global heads up notification snooze
          * @hide
          */
