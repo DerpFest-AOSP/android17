2a84c562b6d9f0635d47b93a8d69a7286257e923
Ghosuto <clash.raja10@gmail.com>
2026-08-11 14:11:34 +0000

    SystemUI: Pulse: Clear stale INSTANCE reference in destroy()

--- packages/SystemUI/src/com/android/systemui/pulse/PulseViewController.kt
+++ packages/SystemUI/src/com/android/systemui/pulse/PulseViewController.kt
@@ -217,6 +217,9 @@ class PulseViewController @Inject constructor(
         audioProcessor.cleanup()
         bassHaptics.reset()
         mainScope.cancel()
+        if (INSTANCE === this) {
+            INSTANCE = null
+        }
     }
 
     companion object {
