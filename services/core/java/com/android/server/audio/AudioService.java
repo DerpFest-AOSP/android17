7e55e50b442720a86eb89e85fa554aff3ebf2b3e
kenway214 <kenway214@outlook.com>
2026-08-17 17:58:15 +0000

    base: Implement dynamic multi-audio focus [1/2]
    Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Change-Id: I91fc9ad9b681d06d1fa8abf01c73bee4fa828c3a
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -1795,7 +1795,7 @@ public class AudioService extends IAudioService.Stub
 
         mPlaybackMonitor.registerPlaybackCallback(mPlaybackActivityMonitor, true);
 
-        mMediaFocusControl = new MediaFocusControl(mPlaybackMonitor, isMultiFocus());
+        mMediaFocusControl = new MediaFocusControl(context, mPlaybackMonitor, isMultiFocus());
 
         readAndSetLowRamDevice();
 
@@ -13119,7 +13119,7 @@ public class AudioService extends IAudioService.Stub
                 Log.w(TAG, "Audio focus environment already exists for token: " + focusEnvToken);
                 return false;
             }
-            MediaFocusControl mfc = new MediaFocusControl(mPlaybackMonitor, isMultiFocus());
+            MediaFocusControl mfc = new MediaFocusControl(mContext, mPlaybackMonitor, isMultiFocus());
             mFocusEnvironmentsMap.put(focusEnvToken, mfc);
             try {
                 focusEnvToken.linkToDeath(mFocusEnvironmentDeathRecipient, 0);
