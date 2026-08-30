d777b849686b9488bea5d135bb216fe652783738
Akash Srivastava <akashniki@gmail.com>
2026-08-30 16:29:59 +0000

    SystemUI: match carrier text font to date one
    (cherry picked from commit 29f21fb8433970c91dbb8d44addcf41caf88621f)

--- packages/SystemUI/compose/features/src/com/android/systemui/shade/ui/composable/ShadeHeader.kt
+++ packages/SystemUI/compose/features/src/com/android/systemui/shade/ui/composable/ShadeHeader.kt
@@ -78,6 +78,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.DeviceFontFamilyName
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.Dp
@@ -804,6 +805,7 @@ private fun CarrierTextNoSubscriptionId(viewModel: ShadeHeaderViewModel) {
             TextStyle(
                 fontFamily =
                     FontFamily(Font(DeviceFontFamilyName("variable-body-medium-emphasized"))),
+                fontWeight = FontWeight.Medium,
                 letterSpacing = 0.01.em,
             ),
         maxLines = 1,
